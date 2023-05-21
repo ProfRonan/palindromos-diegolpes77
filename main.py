987e4487aa8cd7fe91acf0509c2d223db287b692
Diego Lopes <dieolpes77@gmail.com>
2023-05-21 17:25:46 -0300

    feito

--- main.py
+++ main.py
@@ -4,3 +4,10 @@
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
     return False
+
+def is_palindrome(string):
+   
+    cleaned_string = ''.join(c.lower() for c in string if c.isalnum())
+
+   
+    return cleaned_string == cleaned_string[::-1]
